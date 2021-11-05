343ca7ab82c3af9c5f422a9ca93043d2384ed223
Remco-Bruinsma <82671724+Remco-Bruinsma@users.noreply.github.com>
2021-11-05 12:23:10 +0100

    ik heb alles aangepast

---
+++ Webapp-p2-groep-18/Webapp-p2-floraflow/Webapp-p2-floraflow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webapp_p2_floraflow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
